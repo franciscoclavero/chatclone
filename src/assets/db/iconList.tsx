f14db51dfda92391f058743d3ba0ae385e2bc74c
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-05-04 15:30:11 -0300

    feat: create iconList

---
+++ src/assets/db/iconList.tsx
@@ -0,0 +1,14 @@
+export const iconList = [
+  {'name': 'Block', 'src': '../../../public/assets/icons/block.png', 'description': 'Block Icon'},
+  {'name': 'Call', 'src': '../../../public/assets/icons/call.png', 'description': 'call Icon'},
+  {'name': 'Camera', 'src': '../../../public/assets/icons/camera_video.png', 'description': 'camera_video Icon'},
+  {'name': 'Clip', 'src': '../../../public/assets/icons/clip.png', 'description': 'clip Icon'},
+  {'name': 'Close', 'src': '../../../public/assets/icons/close.png', 'description': 'close Icon'},
+  {'name': 'Delete', 'src': '../../../public/assets/icons/delete.png', 'description': 'delete Icon'},
+  {'name': 'Menu', 'src': '../../../public/assets/icons/menu.png', 'description': 'menu Icon'},
+  {'name': 'Mic', 'src': '../../../public/assets/icons/mic.png', 'description': 'mic Icon'},
+  {'name': 'Options', 'src': '../../../public/assets/icons/options.png', 'description': 'options Icon'},
+  {'name': 'Report', 'src': '../../../public/assets/icons/report.png', 'description': 'report Icon'},
+  {'name': 'Search', 'src': '../../../public/assets/icons/search.png', 'description': 'search Icon'},
+  {'name': 'Smile', 'src': '../../../public/assets/icons/smile.png', 'description': 'smile Icon'},
+];
